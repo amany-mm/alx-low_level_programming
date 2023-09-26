c1e06d09ccb1a2e2b2d968bdebf104f29c91a47f
Amany Mohammed <amany.mohammd@gmail.com>
2023-09-26 03:57:47 +0300

    Add a function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 #include <stdio.h> /* size_t, printf */
 #include <stdlib.h> /* malloc */
+
 
 /**
  * struct listint_s - singly linked list
@@ -18,7 +20,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
 /* function prototypes */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
