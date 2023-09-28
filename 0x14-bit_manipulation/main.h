9d4ddf32582c477f31a60a2227a5ef2d910285fe
Amany Mohammed <amany.mohammd@gmail.com>
2023-09-28 21:45:51 +0300

    Add a function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h> /* NULL */
+#include <string.h> /* strlen */
+
+
+/* function prototypes */
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
