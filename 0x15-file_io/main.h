41b42c1edab41264d6fa6d71864d5b521c79a241
Amany Mohammed <amany.mohammd@gmail.com>
2023-10-03 01:51:21 +0300

    Add a function that appends text at the end of a file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,9 +16,9 @@
 /* ssize_t */
 #include <sys/types.h>
 
-
 /* prototype functions */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
